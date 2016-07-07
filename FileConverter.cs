39b75794ed5864117f3c41bd3301654762a9318d
Becky Skillings <beckyskillings@gmail.com>
2016-07-07 16:01:25 -0600

    Working prototype

---
+++ FileConverter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.IO;
+
+namespace IDParserWPF
+{
+    public class FileConverter
+    {
+        public string FindFile()
+        {
+            var openDialog = new OpenFileDialog
+            {
+                DefaultExt = ".txt"
+            };
+
+            bool? result = openDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string inputName = openDialog.FileName;
+                return inputName;
+
+            }
+            return "";
+        }
+
+        public string ConvertFile(string inputName, string outputName)
+        {
+            const string textMarker = "|11=";
+            var random = new Random();
+            
+            //Check if file exists
+            if (!File.Exists(inputName))
+            {
+                Console.WriteLine(inputName + " does not exist");
+                return "";
+            }
+
+            //check if file is empty
+            if (new FileInfo(inputName).Length == 0)
+            {
+                Console.WriteLine(inputName + " is empty");
+                return "";
+            }
+
+
+            //main functionality starts here 
+            using (var reader = new StreamReader(inputName))
+            using (var writer = new StreamWriter(outputName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineComingIn = reader.ReadLine();
+                    var lineGoingOut = lineComingIn;
+                    if (lineComingIn != null && lineComingIn.Contains(textMarker))
+                    {
+                        var markerIndex = lineComingIn.IndexOf(textMarker, StringComparison.Ordinal) + textMarker.Length;
+                        var counter = 0;
+                        var endChar = ' ';
+                        while (!endChar.Equals('|'))
+                        {
+                            counter++;
+                            endChar = lineComingIn[markerIndex + counter];
+                        }
+                        var newNumber = random.Next();
+
+                        var lineModifying = lineComingIn.Remove(markerIndex, counter);
+                        lineGoingOut = lineModifying.Insert(markerIndex, newNumber.ToString());
+                    }
+
+                    writer.WriteLine(lineGoingOut);
+                }
+            }
+            return " Done";
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+
+
+namespace IDParserWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private FileConverter converter = new FileConverter();
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputFile = converter.FindFile();
+            inputFilenameTextbox.Text = inputFile;
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string messageText = converter.ConvertFile(inputFilenameTextbox.Text, outputFilenameTextbox.Text);
+            messageBlock.Text = messageText;
+        }
+
+    }
+}
